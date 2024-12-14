8372c3ae72970e3818b9538ccc5ea16d494baae7
sachindkd <165709511+sachindkd@users.noreply.github.com>
2024-12-14 23:26:07 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function to open the modal
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "flex"; // Display the modal
+}
+
+// Function to close the modal
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none"; // Hide the modal
+}
